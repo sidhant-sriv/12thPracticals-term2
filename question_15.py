54da3180c5cfc5cb3bcd5c704ada67ab18dda80d
Monish Singhal <programminggeekishere@gmail.com>
2022-01-08 11:14:48 +0530

    fix queries to make them cleaner and remove logical errors

--- question_15.py
+++ question_15.py
@@ -10,11 +10,11 @@ mydb = mysql.connector.connect(
 mycursor = mydb.cursor()
 
 # There are two tables CUSTOMER_INFO and  TRANSACTION_DETAIL
-mycursor.execute("SELECT CUSTOMER_INFO.Cust_Name, TRANSACTION_DETAIL.Trans_Id, TRANSACTION_DETAIL.Acc_No,TRANSACTION_DETAIL.Transaction_Type,TRANSACTION_DETAIL.Amount FROM CUSTOMER_INFO, TRANSACTION_DETAIL WHERE CUSTOMER_INFO.Acc_Name = TRANSACTION_DETAIL.Acc_Name AND WHERE CUSTOMER_INFO.Name='Ram'")
+mycursor.execute("SELECT Cust_Name, Trans_Id, TRANSACTION_DETAIL.Acc_No, Transaction_Type, Amount FROM CUSTOMER_INFO JOIN TRANSACTION_DETAIL ON CUSTOMER_INFO.Acc_Name = TRANSACTION_DETAIL.Acc_Name WHERE CUSTOMER_INFO.Name='Ram'")
 for x in mycursor.fetchall():
     print(x)
 # Display the account details and total number of transactions for each account located in “New Delhi”
-mycursor.execute("SELECT CUSTOMER_INFO.Cust_Name, COUNT(TRANSACTION_DETAIL.Trans_Id) FROM CUSTOMER_INFO, TRANSACTION_DETAIL WHERE CUSTOMER_INFO.Acc_Name = TRANSACTION_DETAIL.Acc_Name AND WHERE CUSTOMER_INFO.Add= 'New Delhi'")
+mycursor.execute("SELECT Cust_Name, COUNT(Trans_Id) FROM CUSTOMER_INFO JOIN TRANSACTION_DETAIL ON CUSTOMER_INFO.Acc_Name = TRANSACTION_DETAIL.Acc_Name WHERE CUSTOMER_INFO.Add= 'New Delhi'")
 for x in mycursor.fetchall():
     print(x)
 # Display all records from both the tables using cross join
--- question_16.py
+++ question_16.py
@@ -10,14 +10,14 @@ mydb = mysql.connector.connect(
 mycursor = mydb.cursor()
 # There are two tables FLIGHTS and FARES
 # To display flight number, source, airlines of those flights where fare is less than Rs. 10000.
-mycursor.execute("SELECT FLIGHTS.FNO, FLIGHTS.SOURCE, FLIGHTS.AIRLINES, FARES.FARE FROM FLIGHTS, FARES WHERE FLIGHTS.FNO = FARES.FNO AND FARES.FARE < 10000")
+mycursor.execute("SELECT FLIGHTS.FNO, SOURCE, AIRLINES, FARE FROM FLIGHTS JOIN FARES ON FLIGHTS.FNO = FARES.FNO WHERE FARES.FARE < 10000")
 for x in mycursor.fetchall():
     print(x)
 # To count total no of Indian Airlines flights starting from various cities
-mycursor.execute("SELECT FLIGHTS.SOURCE, COUNT(FLIGHTS.AIRLINES) FROM FLIGHTS WHERE FLIGHTS.AIRLINES = 'Indian Airlines' GROUP BY FLIGHTS.SOURCE")
+mycursor.execute("SELECT SOURCE, COUNT(AIRLINES) FROM FLIGHTS WHERE AIRLINES = 'Indian Airlines' GROUP BY SOURCE")
 for x in mycursor.fetchall():
     print(x)
 # Display all the details where DEST is Varanasi
-mycursor.execute("SELECT FLIGHTS.FNO, FLIGHTS.SOURCE, FLIGHTS.DEST, FLIGHTS.AIRLINES, FARES.FARE FROM FLIGHTS, FARES WHERE FLIGHTS.FNO = FARES.FNO AND FLIGHTS.DEST = 'Varanasi'")
+mycursor.execute("SELECT FLIGHTS.FNO, SOURCE, DEST, AIRLINES, FARE FROM FLIGHTS JOIN FARES ON FLIGHTS.FNO = FARES.FNO WHERE FLIGHTS.DEST = 'Varanasi'")
 for x in mycursor.fetchall():
     print(x)
