afd065321f757572436a1efff1bd42503dc4d3e4
Sandhya C Vijay <97396685+sandhyacv@users.noreply.github.com>
2022-01-11 09:38:51 +0530

    fixed minor typos in question_15.py.

--- question_15.py
+++ question_15.py
@@ -10,11 +10,11 @@ mydb = mysql.connector.connect(
 mycursor = mydb.cursor()
 
 # There are two tables CUSTOMER_INFO and  TRANSACTION_DETAIL
-mycursor.execute("SELECT Cust_Name, Trans_Id, TRANSACTION_DETAIL.Acc_No, Transaction_Type, Amount FROM CUSTOMER_INFO JOIN TRANSACTION_DETAIL ON CUSTOMER_INFO.Acc_Name = TRANSACTION_DETAIL.Acc_Name WHERE CUSTOMER_INFO.Name='Ram'")
+mycursor.execute("SELECT Cust_Name, Trans_Id, TRANSACTION_DETAIL.Acc_No, Transaction_Type, Amount FROM CUSTOMER_INFO JOIN TRANSACTION_DETAIL ON CUSTOMER_INFO.Acc_No = TRANSACTION_DETAIL.Acc_No WHERE CUSTOMER_INFO.Cust_Name='Ram'")
 for x in mycursor.fetchall():
     print(x)
 # Display the account details and total number of transactions for each account located in “New Delhi”
-mycursor.execute("SELECT Cust_Name, COUNT(Trans_Id) FROM CUSTOMER_INFO JOIN TRANSACTION_DETAIL ON CUSTOMER_INFO.Acc_Name = TRANSACTION_DETAIL.Acc_Name WHERE CUSTOMER_INFO.Add= 'New Delhi'")
+mycursor.execute("SELECT Cust_Name, COUNT(Trans_Id) FROM CUSTOMER_INFO JOIN TRANSACTION_DETAIL ON CUSTOMER_INFO.Acc_No = TRANSACTION_DETAIL.Acc_No WHERE CUSTOMER_INFO.Cust_City= 'New Delhi' GROUP BY CUSTOMER_INFO.Cust_Name")
 for x in mycursor.fetchall():
     print(x)
 # Display all records from both the tables using cross join
